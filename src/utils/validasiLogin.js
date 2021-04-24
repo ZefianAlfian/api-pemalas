758261b44f6112aa0dbb8022a4a4c3e12f9da76a
ZefianAlfian <70304409+ZefianAlfian@users.noreply.github.com>
2021-04-25 06:45:21 +0700

    o

--- src/utils/validasiLogin.js
+++ src/utils/validasiLogin.js
@@ -5,16 +5,19 @@ const ErrorResponse = require("../utils/responseError");
 
 module.exports = async function (data, req, res, next) {
   let cek = await cekUserLogin(data);
-  console.log(cek);
-  console.log(data);
-  let cekPwBc = await bcrypt.compare(data.password.toLowerCase(), cek.password);
-  if (cek == null){
-      next(new ErrorResponse("User does not exist", 401))
-      return false;
+  if (cek == null) {
+    next(new ErrorResponse("User does not exist", 401));
+    return false;
   }
-  if (cekPwBc != true){
-      next(new ErrorResponse("Wrong username / email / password", 401))
+  if (cek == true) {
+    let cekPwBc = await bcrypt.compare(
+      data.password.toLowerCase(),
+      cek.password
+    );
+    if (cekPwBc != true) {
+      next(new ErrorResponse("Wrong password", 401));
       return false;
+    }
   }
   if (data.password != data.repeatPassword) {
     next(new ErrorResponse("Passwords are not the same", 401));
@@ -24,10 +27,10 @@ module.exports = async function (data, req, res, next) {
   if (cekPw < 8) {
     next(new ErrorResponse("Password min 8 character", 401));
   }
-  if (cekPwBc){
-      responseMessage(res, 200, "Succses login")
-      req.session.isLogged = true;
-      req.session.dataUser = cek;
-      req.session.save();
+  if (cekPwBc) {
+    responseMessage(res, 200, "Succses login");
+    req.session.isLogged = true;
+    req.session.dataUser = cek;
+    req.session.save();
   }
 };
